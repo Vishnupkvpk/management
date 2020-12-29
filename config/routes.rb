30a35fb8e6cae41541c11e5fd18e64aad3768ff2
Vishnupkvpk <vishnupk213@gmail.com>
2020-12-29 15:43:48 +0530

    third commit

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
   get 'report/show'
   resources :categories
   devise_for :users # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  devise_for :users, controllers: { confirmations: 'confirmations' }
+  # devise_for :users, controllers: { confirmations: 'confirmations' }
   root to: 'home#index'
 end
